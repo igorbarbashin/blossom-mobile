817a92c11db8971802ab985608bc20d26116c7b6
Shawn Anderson <shawn_anderson@sfu.ca>
2018-02-24 13:20:24 -0800

    Install Firebase.

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyA0Po0zGW_PhQrrQ4TAgfjA6c7x75MyatY",
+authDomain: "blossom-mobile.firebaseapp.com",
+databaseURL: "https://blossom-mobile.firebaseio.com",
+projectId: "blossom-mobile",
+storageBucket: "blossom-mobile.appspot.com",
+messagingSenderId: "259425460375"
+};
+firebase.initializeApp(config);
